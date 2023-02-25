93c0eeee7b8fe07a9dcdb65685112e6b01500e22
Zeulni <tobias-zeulner@gmx.de>
2023-02-24 19:11:55 -0500

    comment

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.audio.app_gui import parse_arguments
 
 # TODO: create new requirements.txt, create readme etc.
 
+# video_path is used to run with one call over multiple videos (for the normal usage this is not needed)
 def main(video_path = None) -> None:
     args = parse_arguments()
     runner = Runner(args, video_path)
