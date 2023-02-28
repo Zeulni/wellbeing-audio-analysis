ac1bfd1fe939a99afdb0dd0284275f2074530935
Zeulni <tobias-zeulner@gmx.de>
2023-02-28 13:27:52 -0500

    In faces_id is now only stored one image per detected person (-> makes debugging and identifying easier)

--- src/audio/ASD/speaker_diarization.py
+++ src/audio/ASD/speaker_diarization.py
@@ -95,11 +95,11 @@ class SpeakerDiarization:
         #   (-> if bounding boxes are close by and do not speak at the same time, then they are from the same person)
         # 	To correct for that, I would have to leverage face verification
         
-        # Store one image per track in a folder
-        self.store_face_ids(self.faces_id_path, tracks)
-        
         # Calculate tracks that belong together based on face embeddings
         same_tracks = self.cluster_tracks.cluster_tracks_face_embedding(track_speaking_faces, tracks)
+        
+        # Store one image per track in a folder
+        self.store_face_ids(self.faces_id_path, tracks, same_tracks)
 
         # Old clustering based on assumption, that people do not change the place during one video
         # same_tracks = self.cluster_tracks.cluster_tracks(tracks, all_faces, track_speaking_faces)
@@ -109,7 +109,7 @@ class SpeakerDiarization:
         
         return
     
-    def store_face_ids(self, faces_id_path, tracks) -> None:
+    def store_face_ids(self, faces_id_path, tracks, same_tracks) -> None:
         # Store in a dict for each track the track id, the frame number of the first frame and the corresponding bounding box
         track_bboxes = [None for i in range(len(tracks))]
         for tidx, track in enumerate(tracks):
@@ -117,8 +117,16 @@ class SpeakerDiarization:
             frame_number = -10
             track_bboxes[tidx] = [track['track']['frame'][frame_number] ,track['proc_track']['s'][frame_number], track['proc_track']['x'][frame_number], track['proc_track']['y'][frame_number]]
             
+        # From the list same_tracks, extract from every list it contains the first element and store it in a list
+        cluster_ids = [x[0] for x in same_tracks]    
+            
         # Using faces_id dict, now crop for each track the bounding box at the corresponding frame and save it as an image            
         for tidx, track_bbox in enumerate(track_bboxes):
+            
+            # Only store one picture per recognized cluster
+            if tidx not in cluster_ids:
+                continue
+            
             cap = cv2.VideoCapture(self.video_path)
             cap.set(1, track_bbox[0])
             ret, frame = cap.read()
