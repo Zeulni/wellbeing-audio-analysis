2cd6514626b5cdfaf21034914e3827a904bff30b
Zeulni <tobias-zeulner@gmx.de>
2023-02-28 09:13:43 -0500

    Implemented storing 5 random images per track in a new folder

--- src/audio/ASD/cluster_tracks.py
+++ src/audio/ASD/cluster_tracks.py
@@ -11,10 +11,85 @@ from sklearn.decomposition import PCA
 from insightface.app import FaceAnalysis
 
 class ClusterTracks:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, tracks_faces_clustering_path, video_path, crop_scale) -> None:
+        self.tracks_faces_clustering_path = tracks_faces_clustering_path
+        self.video_path = video_path
+        self.crop_scale = crop_scale
     
-    def cluster_tracks_face_embedding(self, track_speaking_faces, faces_id_path, tracks):
+    def store_face_verification_track_images(self, tracks) -> None:
+        # Store in a dict for each track the track id, the frame number of the first frame and the corresponding bounding box
+        # track_bboxes = [None for i in range(len(tracks))]
+        # for tidx, track in enumerate(tracks):
+        #     # Take the one frame of every track and store it
+        #     frame_number = -10
+        #     track_bboxes[tidx] = [track['track']['frame'][frame_number] ,track['proc_track']['s'][frame_number], track['proc_track']['x'][frame_number], track['proc_track']['y'][frame_number]]
+         
+        # Instead of getting just one frame per track, get 5 random frames per track
+        track_bboxes = []
+        for tidx, track in enumerate(tracks):
+            # Take the one frame of every track and store it
+            
+            # Get 5 random numbers from the array track['track']['frame']
+            frame_numbers = np.random.choice(track['track']['frame'], 5, replace=False)
+            
+            # Get the indices of the frame numbers in the track['track']['frame'] array
+            frame_numbers = np.searchsorted(track['track']['frame'], frame_numbers)
+            
+            track_bboxes.append([])
+            
+            for frame_number in frame_numbers:
+                track_bboxes[tidx].append([track['track']['frame'][frame_number] ,track['proc_track']['s'][frame_number], track['proc_track']['x'][frame_number], track['proc_track']['y'][frame_number]])
+         
+            
+        # Using faces_id dict, now crop for each track the bounding box at the corresponding frame and save it as an image            
+        for tidx, track_bbox in enumerate(track_bboxes):
+            
+            # Make a 4 digit number of out the ID (e.g. 1 -> 001)
+            track_id = str(tidx)
+            if len(track_id) == 1:
+                track_id = "000" + track_id
+            elif len(track_id) == 2:
+                track_id = "00" + track_id
+            elif len(track_id) == 3:    
+                track_id = "0" + track_id
+            
+            # Create for each track a folder  
+            track_folder_path = os.path.join(self.tracks_faces_clustering_path, track_id)
+            if not os.path.exists(track_folder_path):
+                os.makedirs(track_folder_path)
+                
+            # Loop over the 5 frames of the track
+            for i, frame_number in enumerate(track_bbox):
+            
+                cap = cv2.VideoCapture(self.video_path)
+                cap.set(1, frame_number[0])
+                ret, frame = cap.read()
+                cap.release()
+                
+                # Using the bbox from the faces_id dict to crop the image
+                
+                s = frame_number[1]
+                x = frame_number[2]
+                y = frame_number[3]
+                
+                cs  = self.crop_scale
+                bs  = s   # Detection box size
+                bsi = int(bs * (1 + 2 * cs))  # Pad videos by this amount 
+                frame = np.pad(frame, ((bsi,bsi), (bsi,bsi), (0, 0)), 'constant', constant_values=(110, 110))
+                my  = y + bsi  # BBox center Y
+                mx  = x + bsi  # BBox center X
+                face = frame[int(my-bs):int(my+bs*(1+2*cs)),int(mx-bs*(1+cs)):int(mx+bs*(1+cs))]
+
+                
+                # Save the image
+                file_name = track_id + "_" + str(i) + ".jpg"
+                
+                cv2.imwrite(os.path.join(track_folder_path, file_name), cv2.resize(face, (224, 224)))
+    
+    def cluster_tracks_face_embedding(self, track_speaking_faces, tracks):
+        
+        self.store_face_verification_track_images(tracks)
+        
         # Model will automatically be downloaded if not present
         
         # model = insightface.model_zoo.get_model(model_file)
@@ -24,16 +99,15 @@ class ClusterTracks:
         # Downloading smaller model manually if want to use it (but worse performance)
         # buffalo_sc https://drive.google.com/file/d/19I-MZdctYKmVf3nu5Da3HS6KH5LBfdzG/view?usp=sharing
         
-        
-        # TODO: if it works, then change folder where it is stored
-        
         # Check if a file naming "model.pkl" exists in the folder faces_id_path (if yes, then load the pkl file), otherwise initialize the model
         
-        if os.path.isfile(os.path.join(faces_id_path, 'model.pkl')):
-            model = pickle.load(open(os.path.join(faces_id_path, 'model.pkl'), 'rb'))
+        model_path = os.path.join(self.tracks_faces_clustering_path, 'model.pkl')
+        
+        if os.path.isfile(model_path):
+            model = pickle.load(open(model_path, 'rb'))
         else:
             model = FaceAnalysis("buffalo_l", providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
-            pickle.dump(model, open(os.path.join(faces_id_path, 'model.pkl'), 'wb'))
+            pickle.dump(model, open(model_path, 'wb'))
         
         model.prepare(ctx_id=0, det_size=(224, 224))
         
@@ -42,17 +116,24 @@ class ClusterTracks:
         # embeddings = np.zeros((len(tracks),512))
         embeddings = {}
         
-        sorted_files = sorted(os.listdir(faces_id_path))
-        # Filter the files to only include the jpg files
-        sorted_files = [file for file in sorted_files if file.endswith('.jpg')]
+        # sorted_files = sorted(os.listdir(model_path))
+        # # Filter the files to only include the jpg files
+        # sorted_files = [file for file in sorted_files if file.endswith('.jpg')]
         
-        for track_id in sorted_files:
-            # Check if the image is a jpg file
-            img = cv2.imread(os.path.join(faces_id_path, track_id))
-            face = model.get(img)
-            embedding = face[0].normed_embedding
-            embeddings[track_id] = embedding
-            # embeddings[i] = embedding
+        # for track_id in sorted_files:
+        #     # Check if the image is a jpg file
+        #     img = cv2.imread(os.path.join(faces_id_path, track_id))
+        #     face = model.get(img)
+        #     embedding = face[0].normed_embedding
+        #     embeddings[track_id] = embedding
+        #     # embeddings[i] = embedding
+        
+        # Go through each of the folders in self.tracks_faces_clustering_path, and for each folder, go through each of the images in the folder and calculate the embedding
+        # Then, average the embeddings of the images in the folder and store the average embedding in the embeddings dict
+        sorted_files = sorted(os.listdir(self.tracks_faces_clustering_path))
+        # Filter the files to only include the jpg files 
+        sorted_files = [file for file in sorted_files if file.isdigit()]
+        
 
         embeddings_list = list(embeddings.values())
 
