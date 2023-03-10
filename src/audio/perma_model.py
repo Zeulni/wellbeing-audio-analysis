925b8f4498776663ef8c4583562d52dcffc7f367
Zeulni <tobias-zeulner@gmx.de>
2023-03-10 16:08:00 -0500

    780 features per time series added via tsfresh

--- src/audio/perma_model.py
+++ src/audio/perma_model.py
@@ -11,29 +11,52 @@ class PermaModel:
     def __init__(self):
         self.times_series_features = TimeSeriesFeatures()
     
+    # Data Set creation
+    # Input: Team level (only for the speaker I have the informed consent - not checking for PERMA yet)
+    # - Option 1: the time series data per day
+    # - Option 2: the time series data for all 3 days (then the ID has to be different -> e.g. "1_1", "1_2", "1_3" for the first speaker on the first day, second day, third day)
+        # team 1, speaker 1, day 1 -> 11_1, 11_2, 11_3
+    # Output: 
+    # - Option 1: One CSV file per team (PERMA score matching manually afterwards)
+    # - Option 2: One CSV file per team including the PERMA scores (matched automatically by speaker ID, e.g. 11_1, 11_2, 11_3)
+    
+    # Afterwards:
+    # - Concat all teams into one CSV file
+    # - Standardize and normalize the data
+    # - Train model
+    # - Save normalization, standardication, and model function in a pickle file
     def calculate_features(self):
         
         # Read in the CSV file
         csv_path = PERMA_MODEL_TRAINING_DATA / 'test.csv'
+        # csv_path = PERMA_MODEL_TRAINING_DATA / 'test_sample.csv'
         
         # Read the pandas dataframe and set "Speaker ID" as index
         df = pd.read_csv(csv_path)
-
-        # Get the columns containing the emotions, and the communication patterns (each a different dataframe)
-        # x -> value over time, y -> speaker ID
-        # df_dominance = df[[col for col in df.columns if col.startswith('dominance')]]
+        # For test_sample, use ";" as delimiter
+        # df = pd.read_csv(csv_path, delimiter=';')
         
-        # TODO: make it more modular (e.g. by providing list of features in config file -> but model does only work with these features?)
-        # TODO: sample check if features are calculated correctly (e.g. with sample input data)
-        overall_df = self.times_series_features.calc_statistical_features(df)
+        feature_names = ['arousal', 'dominance', 'valence', 
+                         'norm_num_overlaps_absolute', 'norm_num_overlaps_relative', 
+                         'norm_num_turns_absolute', 'norm_num_turns_relative', 
+                         'norm_speak_duration_absolute', 'norm_speak_duration_relative']
+        
+        # For each speaker: 9 time series x 5 features = 45 features
+        short_overall_df, long_overall_df = self.times_series_features.calc_time_series_features(df, feature_names)
 
         print("test")
+        
+        # Save the short and long time series features in a csv file
+        short_overall_df.to_csv(PERMA_MODEL_TRAINING_DATA / 'short_overall_df.csv')
+        long_overall_df.to_csv(PERMA_MODEL_TRAINING_DATA / 'long_overall_df.csv')
+
 
         # Save values in a csv (PERMA score can then be added manually)
         # -> one csv per day containing for each speaker (9 x 4-5) ~45 input features + PERMA score
         # Speaker ID for training not necessary (only for matching to PERMA score), but for inference it is (-> to assign PERMA score to correct Person)
 
         # TODO: Once added all x and y (PERMA scores) to a csv, then also standardize and normalize the x values??
+        # TODO: Especially for long_overall -> filter out columns with NaN or 0s
         
     # TODO: regarding PERMA score: standardize and normalize
     def calculate_perma_score(self):
--- src/audio/utils/time_series_features.py
+++ src/audio/utils/time_series_features.py
@@ -1,32 +1,27 @@
 import numpy as np
 import pandas as pd
-
-from sklearn.linear_model import LinearRegression
+from tsfresh import extract_features
 
 class TimeSeriesFeatures:
     def __init__(self):
         pass
     
-    def calc_statistical_features(self, df):
-        df_dominance = self.calc_ind_feature(df, 'dominance')
-        df_arousal = self.calc_ind_feature(df, 'arousal')
-        df_valence = self.calc_ind_feature(df, 'valence')
-        df_norm_num_overlaps_absolute = self.calc_ind_feature(df, 'norm_num_overlaps_absolute')
-        df_norm_num_overlaps_relative = self.calc_ind_feature(df, 'norm_num_overlaps_relative')
-        df_norm_num_turns_absolute = self.calc_ind_feature(df, 'norm_num_turns_absolute')
-        df_norm_num_turns_relative = self.calc_ind_feature(df, 'norm_num_turns_relative')
-        df_norm_speak_duration_absolute = self.calc_ind_feature(df, 'norm_speak_duration_absolute')
-        df_norm_speak_duration_relative = self.calc_ind_feature(df, 'norm_speak_duration_relative')
-
-        # Concatenate the dataframes (per person per PERMA score ~45 features), based on Speaker ID
-        overall_df = pd.concat([df_dominance, df_arousal, df_valence, df_norm_num_overlaps_absolute, df_norm_num_overlaps_relative, df_norm_num_turns_absolute, df_norm_num_turns_relative, df_norm_speak_duration_absolute, df_norm_speak_duration_relative], axis=1)
+    def calc_time_series_features(self, df, feature_names):        
+        # Make it more modular (e.g. by providing list of features)
+        short_overall_df = pd.DataFrame()
+        long_overall_df = pd.DataFrame()
+        for feature_name in feature_names:
+            short_feature_df = self.calc_ind_feature(df, feature_name)
+            short_overall_df = pd.concat([short_overall_df, short_feature_df], axis=1)
         
-        return overall_df
+            long_feature_df = self.calc_tsfresh_features(df, feature_name)
+            long_overall_df = pd.concat([long_overall_df, long_feature_df], axis=1)
+            
+        return short_overall_df, long_overall_df
     
     def calc_ind_feature(self, df, feature_name):
         # Calculate statistics for feature columns (e.g. dominance, valence, arousal)
         feature_cols = [col for col in df.columns if col.startswith(feature_name)]
-        # feature_stats = ['mean', 'slope', 'min', 'max', 'std']
 
         feature_values = df[feature_cols].values
         feature_mean = np.mean(feature_values, axis=1)
@@ -34,27 +29,16 @@ class TimeSeriesFeatures:
         feature_min = np.min(feature_values, axis=1)
         feature_max = np.max(feature_values, axis=1)
         feature_std = np.std(feature_values, axis=1)
-
-        # Create new dataframe with calculated statistics
-        # new_df = pd.DataFrame({
-        #     'Speaker ID': df['Speaker ID'],
-        #     'mean': feature_mean,
-        #     'slope': feature_slope,
-        #     'min': feature_min,
-        #     'max': feature_max,
-        #     'std': feature_std
-        # })
         
-        # The column names contain the statistic (e.g. mean) and the feature name (e.g. dominance)
+        # The column names contain the statistic (e.g. mean) and the feature name (e.g. dominance)        
         new_df = pd.DataFrame({
             'Speaker ID': df['Speaker ID'],
-            'mean_' + feature_name: feature_mean,
-            'slope_' + feature_name: feature_slope,
-            'min_' + feature_name: feature_min,
-            'max_' + feature_name: feature_max,
-            'std_' + feature_name: feature_std
-        })
-        
+            feature_name + '_mean': feature_mean,
+            feature_name + '_slope': feature_slope,
+            feature_name + '_min': feature_min,
+            feature_name + '_max': feature_max,
+            feature_name + '_std': feature_std
+        })  
         
         # Set the speaker ID as index
         new_df.set_index('Speaker ID', inplace=True)
@@ -65,15 +49,27 @@ class TimeSeriesFeatures:
         x = np.arange(len(feature_values))
         slope, _ = np.polyfit(x, feature_values, 1)
         return slope
+    
+    
+    def calc_tsfresh_features(self, df, feature_name):
+        # extract the column names that start with "arousal_"
+        value_vars = df.filter(regex='^'+feature_name).columns.tolist()
 
-    # def calculate_slope(self, feature_values):
-    #     # Create a linear regression object
-    #     lr = LinearRegression()
+        # use the melt function to transform the dataframe to a timeseries format
+        id_vars = ['Speaker ID']
+        df_timeseries = pd.melt(df, id_vars=id_vars, value_vars=value_vars, var_name='time', value_name=feature_name)
 
-    #     # Fit a linear regression line to the feature values
-    #     lr.fit(feature_values[:, :-1], feature_values[:, -1])
+        # sort the dataframe by speaker id and time
+        df_timeseries = df_timeseries.sort_values(by=['Speaker ID', 'time']).reset_index(drop=True)
+ 
+        df_features = extract_features(df_timeseries, column_id='Speaker ID', column_sort='time')
+        
+        # Name the index "Speaker ID"
+        df_features.index.name = 'Speaker ID'
+        
+        # print the resulting dataframe
+        # print(df_features.head())
 
-    #     # Get the slope of the fitted line
-    #     slope = lr.coef_[0]
-
-    #     return slope
+        # df_features.to_csv('features.csv')
+        
+        return df_features
