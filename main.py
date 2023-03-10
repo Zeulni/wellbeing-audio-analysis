e1efc343b602dc57f26aa1d8ef4542cd53b1b558
Zeulni <tobias-zeulner@gmx.de>
2023-03-10 18:01:25 -0500

    up-to-date version

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from src.audio.perma_model import PermaModel
 
 # video_path is used to run with one call over multiple videos (for the normal usage this is not needed)
 def main(video_path = None) -> None:
-    # args = parse_arguments()
-    # runner = Runner(args, video_path)
-    # runner.run()
+    args = parse_arguments()
+    runner = Runner(args, video_path)
+    runner.run()
 
-    perma_model = PermaModel()
-    perma_model.calculate_features()
+    # perma_model = PermaModel()
+    # perma_model.calculate_features()
 
 if __name__ == "__main__":
     main()
