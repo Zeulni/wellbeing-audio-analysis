current best models (added ridge, fixed bug with variance cor, and added plot for corr matrix)

src/audio/perma_model/perma_regressor.py
@@ -6,6 +6,7 @@ import shap
 from catboost import CatBoostRegressor
 import xgboost as xgb
 from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
 
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.multioutput import MultiOutputRegressor
@@ -14,6 +15,7 @@ from sklearn.model_selection import LeaveOneOut
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import make_scorer, r2_score
 from sklearn.model_selection import KFold
+import multiprocessing
 
 
 from math import sqrt
@@ -21,18 +23,63 @@ from math import sqrt
 from src.audio.utils.constants import PERMA_MODEL_RESULTS_DIR
 
 class PermaRegressor:
-    def __init__(self, data_X, data_y, database_name) -> None:
+    def __init__(self, data_X, data_y, perma_feature_list, database_name) -> None:
         # self.data_X = data_X
         # self.data_y = data_y
         
         # Divide into train and test set (shuffle=True)
-        self.data_X_train, self.data_X_test, self.data_y_train, self.data_y_test = train_test_split(data_X, data_y, test_size=0.2, random_state=42)
+        self.data_X_train, self.data_X_test, self.data_y_train, self.data_y_test = train_test_split(data_X, data_y, test_size=0.15, random_state=42)
+        
+        self.perma_feature_list = perma_feature_list
         
         self.database_name = database_name
         
+        self.baseline_comp_dict = {"ridge": {"baseline": [], "prediction": []},
+                                   "lasso": {"baseline": [], "prediction": []},
+                                   "xgboost": {"baseline": [], "prediction": []},
+                                   "catboost": {"baseline": [], "prediction": []}}
+        
+        
+        # * Ridge Model
+        self.ridge_param_grid = {
+            'alpha': [0.001, 0.005, 0.01, 0.02, 0.04, 0.06, 0.08, 0.1]
+        }
+        self.ridge_reg_model = Ridge()
+        
+        # * Lasso Model
+        self.lasso_param_grid = {
+            'alpha': [0.001, 0.005, 0.01, 0.02, 0.04, 0.06, 0.08, 0.1]
+        }
+        self.lasso_reg_model = Lasso()
+        
+        # * CatBoost Model
+        self.catboost_param_grid = {
+            'max_depth': [3, 5, 7],
+            'learning_rate': [0.1, 0.01],
+            'n_estimators': [100, 200]
+        }
+        self.catboost_reg_model = CatBoostRegressor(loss_function='RMSE' ,verbose=False, save_snapshot=False, allow_writing_files=False, train_dir=str(PERMA_MODEL_RESULTS_DIR))
+        
+        # * XGBoost Model
+        self.xgboost_param_grid = {
+            'max_depth': [3, 5, 7],
+            'learning_rate': [0.1, 0.01],
+            'n_estimators': [100, 200]
+        }
+        self.xgboost_reg_model = xgb.XGBRegressor(objective='reg:squarederror')  
+        
+        # Create the lists with params and models
+        # self.model_name_list = ["ridge", "lasso", "xgboost", "catboost"]
+        # self.model_param_grid_list = [self.ridge_param_grid, self.lasso_param_grid, self.xgboost_param_grid, self.catboost_param_grid]
+        # self.reg_model_list = [self.ridge_reg_model, self.lasso_reg_model, self.xgboost_reg_model, self.catboost_reg_model]
+        
+        self.model_name_list = ["ridge", "lasso"]
+        self.model_param_grid_list = [self.ridge_param_grid, self.lasso_param_grid]
+        self.reg_model_list = [self.ridge_reg_model, self.lasso_reg_model]
+        
+        
     def train_model(self, multioutput_reg_model, model_name, param_grid):        
         
-        # TODO: loo back?
         loo = LeaveOneOut() 
         rmse = "neg_root_mean_squared_error"
         grid_search = GridSearchCV(multioutput_reg_model, param_grid, cv=loo, scoring=rmse, verbose=0, n_jobs=-1, refit=rmse)
@@ -62,18 +109,18 @@ class PermaRegressor:
     def train_ind_models(self, reg_model, model_name, param_grid):        
         
         models = []
-        for i in range(self.data_y_train.shape[1]):
-            models.append(reg_model)
+        # for i in range(self.data_y_train.shape[1]):
+        #     models.append(reg_model)
         
 
         
         best_params = []
         best_scores = []
         for i in range(self.data_y_train.shape[1]):
-            model = models[i]
+            model = reg_model
             # scorer = ["neg_root_mean_squared_error", "r2"]
-            scoring = "neg_root_mean_squared_error"
-            # scoring = 'neg_mean_absolute_error'
+            # scoring = "neg_root_mean_squared_error"
+            scoring = 'neg_mean_absolute_error'
             # scoring = "neg_mean_squared_error"
             # scoring = "r2"
             # rmse_scorer = make_scorer(lambda y_true, y_pred: sqrt(mean_squared_error(y_true, y_pred)), greater_is_better=False)
@@ -81,67 +128,128 @@ class PermaRegressor:
             # cv = KFold(n_splits=cv_factor, shuffle=True, random_state=42)
             cv = LeaveOneOut() 
             grid_search = GridSearchCV(model, param_grid, cv=cv, scoring=scoring, verbose=0, n_jobs=-1, refit=scoring)
-            grid_search.fit(self.data_X_train, self.data_y_train.iloc[:, i])
+            grid_search.fit(self.data_X_train[self.perma_feature_list[i]], self.data_y_train.iloc[:, i])
             # best_alpha = grid_search.best_params_['alpha']
             # models.append(Lasso(alpha=best_alpha))
             # models_trained.append(grid_search.best_estimator_)
-            models[i] = grid_search.best_estimator_
+            models.append(grid_search.best_estimator_)
             best_params.append(grid_search.best_params_)
             # Train model with best alpha
             # models[i].fit(self.data_X, self.data_y.iloc[:, i])
             best_scores.append(-grid_search.best_score_)
             
+            print("Best score for " + model_name + " : ", round(-grid_search.best_score_,3))
+            
+            self.calc_baseline_comparison(models[i], model_name, i)
+            
         # Print the sum and mean of the best scores
-        print(model_name + " Mean Best Score " + scoring + ": ", np.mean(best_scores))
-        print(model_name + " Best Params: ", best_params)
+        # print(model_name + " Mean Best Score " + scoring + ": ", np.mean(best_scores))
+        # print(model_name + " Best Params: ", best_params)
         
-        multioutput_reg_model = MultiOutputRegressor(Lasso())
+        # multioutput_reg_model = MultiOutputRegressor(Lasso())
         
-        # Set estimators_ attribute of MultiOutputRegressor object
-        multioutput_reg_model.estimators_ = models
+        # # Set estimators_ attribute of MultiOutputRegressor object
+        # multioutput_reg_model.estimators_ = models
         
-        # Print baseline RMSE and MAE
-        self.calc_baseline_comparison(multioutput_reg_model, model_name)
+        # # Print baseline RMSE and MAE
+        # self.calc_baseline_comparison(multioutput_reg_model, model_name)
         
-        self.plot_and_save_feature_importance(multioutput_reg_model, model_name)
-        self.plot_and_save_shap_values(multioutput_reg_model, model_name)
+        # self.plot_and_save_feature_importance(multioutput_reg_model, model_name)
+        # self.plot_and_save_shap_values(multioutput_reg_model, model_name)
     
-        # Save models dict to pickle file
-        model_file_name = self.database_name + '_' + model_name + '_perma_model.pkl'
-        with open(PERMA_MODEL_RESULTS_DIR / model_file_name, 'wb') as f:
-            pkl.dump(multioutput_reg_model, f)
+        # # Save models dict to pickle file
+        # model_file_name = self.database_name + '_' + model_name + '_perma_model.pkl'
+        # with open(PERMA_MODEL_RESULTS_DIR / model_file_name, 'wb') as f:
+        #     pkl.dump(multioutput_reg_model, f)
 
-    def calc_baseline_comparison(self, multioutput_reg_model, model_name):
+    def train_multiple_models(self):
         
-        # TODO: Baseline based on train set mean!?
-        # Calculate the mean of the PERMA scores
-        mean_perma_scores = self.data_y_train.mean(axis=0)
+        for i in range(len(self.model_name_list)):
+            self.train_ind_models(self.reg_model_list[i], self.model_name_list[i], self.model_param_grid_list[i])
         
-        y_pred_baseline = np.tile(mean_perma_scores, (len(self.data_y_test), 1))
+        # Use multiprocessing to train the models in parallel
+        # with multiprocessing.Pool() as pool:
+        #     pool.starmap(self.train_ind_models, zip(self.reg_model_list, self.model_name_list, self.model_param_grid_list))
+
+        self.plot_baseline_comparison()
+
+    def calc_baseline_comparison(self, reg_model, model_name, y_i):
+        
+        data_y_train = self.data_y_train.iloc[:, y_i]
+        data_y_test = self.data_y_test.iloc[:, y_i]
+        
+        # TODO: Baseline based on train set mean!?        
+        # Select only the y_i column and then calculate the mean of the PERMA scores
+        mean_perma_scores = data_y_train.mean(axis=0)
+        
+        y_pred_baseline = np.tile(mean_perma_scores, (len(data_y_test), 1))
         
         # Calculate the RMSE of the baseline model
-        baseline_rmse = np.sqrt(mean_squared_error(self.data_y_test, y_pred_baseline))
+        baseline_rmse = np.sqrt(mean_squared_error(data_y_test, y_pred_baseline))
         
         # Calculate the R2 score of the baseline model
-        baseline_r2 = r2_score(self.data_y_test, y_pred_baseline)
+        baseline_r2 = r2_score(data_y_test, y_pred_baseline)
         
         # Calculate the MAE of the baseline model
-        baseline_mae = mean_absolute_error(self.data_y_test, y_pred_baseline)
+        baseline_mae = mean_absolute_error(data_y_test, y_pred_baseline)
         
-        print(model_name + " Test Set Baseline r2:", round(baseline_r2, 3))
-        print(model_name + " Test Set Baseline RMSE:", round(baseline_rmse,3))
+        # print(model_name + " Test Set Baseline r2:", round(baseline_r2, 3))
+        # print(model_name + " Test Set Baseline RMSE:", round(baseline_rmse,3))
         print (model_name + " Test Set Baseline MAE:", round(baseline_mae,3))
+        self.baseline_comp_dict[model_name]["baseline"].append(baseline_mae)
         
-        # Using the entire dataset as test set (with multioutput_reg_model) to comput R2, RMSE, MAE
-        y_pred = multioutput_reg_model.predict(self.data_X_test)
-        r2 = r2_score(self.data_y_test, y_pred)
-        rmse = np.sqrt(mean_squared_error(self.data_y_test, y_pred))
-        mae = mean_absolute_error(self.data_y_test, y_pred)
+        # Using the entire dataset as test set (with reg_model) to comput R2, RMSE, MAE
+        y_pred = reg_model.predict(self.data_X_test[self.perma_feature_list[y_i]])
+        r2 = r2_score(data_y_test, y_pred)
+        rmse = np.sqrt(mean_squared_error(data_y_test, y_pred))
+        mae = mean_absolute_error(data_y_test, y_pred)
         
-        print(model_name + " Test Set Prediction r2:", round(r2,3))
-        print(model_name + " Test Set Prediction RMSE:", round(rmse,3))
+        # print(model_name + " Test Set Prediction r2:", round(r2,3))
+        # print(model_name + " Test Set Prediction RMSE:", round(rmse,3))
         print(model_name + " Test Set Prediction MAE:", round(mae,3))
+        self.baseline_comp_dict[model_name]["prediction"].append(mae)
         
+    def plot_baseline_comparison(self):
+        
+        # Initialize empty lists to store the model names and average values
+        models = []
+        baseline_avgs = []
+        prediction_avgs = []
+
+        # Iterate over the keys in the baseline_comp_dict dictionary to extract the model names and values
+        for model, values in self.baseline_comp_dict.items():
+            models.append(model)
+            baseline_avgs.append(np.mean(values["baseline"]))
+            prediction_avgs.append(np.mean(values["prediction"]))
+
+        # Set up the plot
+        fig, ax = plt.subplots()
+        bar_width = 0.35
+        opacity = 0.8
+        index = np.arange(len(models))
+
+        # Create the bars for the baseline values
+        rects1 = ax.bar(index, baseline_avgs, bar_width,
+                        alpha=opacity,
+                        color='b',
+                        label='Baseline')
+
+        # Create the bars for the prediction values
+        rects2 = ax.bar(index + bar_width, prediction_avgs, bar_width,
+                        alpha=opacity,
+                        color='g',
+                        label='Prediction')
+
+        # Add labels, title and legend to the plot
+        ax.set_xlabel('Models')
+        ax.set_ylabel('Average MAE over all 5 PERMA pillars')
+        ax.set_title('Baseline and Prediction Averages by Model')
+        ax.set_xticks(index + bar_width / 2)
+        ax.set_xticklabels(models)
+        ax.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+
+        # Show the plot
+        plt.show()
     
     def catboost_train(self):
         